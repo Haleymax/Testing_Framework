581948cef73bf9ffa46804082578b9b10c86b03a
hongwei.huang <hongwei.huang@unity.cn>
2024-09-25 11:33:14 +0800

    add something function

--- core/client.py
+++ core/client.py
@@ -1,11 +1,18 @@
 import time
 
+from Tools.scripts.make_ctype import method
 from retry import retry
+from scripts.regsetup import description
 from yaml import NodeEvent
 import uiautomator2 as u2
 
 from core.logger import logger
 
+class By:
+    Text = "text"
+    ResourceId = "resourceId"
+    Description = "description"
+    ClassName = "className"
 
 class Client :
 
@@ -40,5 +47,37 @@ class Client :
     def stop_app(self , packname):
         self.driver.app_stop(packname)
 
+    def get_elem(self , locator):
+        method , l = locator
+
+        if method == By.Text:
+            if self.driver(text=l).exists:
+                return self.driver(text=l)
+        if method == By.ResourceId:
+            if self.driver(resourceId=l).exists:
+                return self.driver(resourceId=l)
+        if method == By.Description:
+            if self.driver(description=l).exists:
+                return self.driver(description=l)
+        if method == By.ClassName:
+            if self.driver(class_name=l).exists:
+                return self.driver(class_name=l)
+        return None
+
+def click(self , locator) :
+    retries = 3
+    delay = 2
+    for attempt in range(3):
+        element = self.get_elem(locator)
+        if element is not None:
+            element.click()
+            return
+        else:
+            logger.info(f"Attempt {attempt + 1} failed")
+            if attempt < retries-1:
+                time.sleep(delay)
+    raise Exception(f"{logger} not found")
+
+
     def send_text(self , locator , value):
-        self.ge
+        self.get_elem(locator).set_text(value)
