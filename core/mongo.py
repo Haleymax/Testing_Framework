0807eff8b376e11cc9968053967dc0dc5d5ff25b
hongwei.huang <hongwei.huang@unity.cn>
2024-09-26 17:51:04 +0800

    operation of mongodb

--- core/mongo.py
+++ core/mongo.py
@@ -8,5 +8,18 @@ class MongoClint:
         self.client = pymongo.MongoClient(url)
         self.db = self.client[database]
 
+    def insert_document(self, collection_name, document):
+        collection = self.db[collection_name]
+        return collection
 
+    def find_document(self, collection_name, key):
+        collection = self.db[collection_name]
+        return collection.find(key)
 
+    def update_document(self, collection_name, query, update_data):
+        collection = self.db[collection_name]
+        return collection.update_one(query, update_data)
+
+    def delete_document(self, collection_name, query):
+        collection = self.db[collection_name]
+        return collection.delete_one(query)
