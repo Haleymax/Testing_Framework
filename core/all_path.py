2dc131c9a29f8dd6bea7558fa274548ebaad8416
hongwei.huang <hongwei.huang@unity.cn>
2024-09-26 09:55:10 +0800

    requiregitlabpackage

--- core/all_path.py
+++ core/all_path.py
@@ -2,6 +2,6 @@ import os.path
 
 base_path = os.path.dirname(os.path.dirname(__file__))
 
-addPath = os.path.join(base_path , "app")
+appPath = os.path.join(base_path , "app")
 dataPath = os.path.join(base_path , "data" , "webgl_host")
 reportPath = os.path.join(base_path , "report")
