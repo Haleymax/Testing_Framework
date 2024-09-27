6e6b325920c15b901c489a38eb5b7dc9872b5d89
hongwei.huang <hongwei.huang@unity.cn>
2024-09-27 16:09:20 +0800

    spell data

--- testcases/test_webghost/conftest.py
+++ testcases/test_webghost/conftest.py
@@ -28,12 +28,32 @@ def get_collection():
     return collection
 
 
-def get_data(get_mongourl, get_database, get_collection):
+def get_data(get_mongourl,get_database,get_collection):
     client = MongoClint()
     mogourl = get_mongourl
     database = get_database
     collection = get_collection
     client.connect(mogourl)
     client.use_database(database)
+    data = client.find_collection(collection)
+    result = []
+    webgl_host_data = {}
+    for x in data :
+        linedata = []
+        deviceid = x['deviceid']
+        packagename = x['packagename']
+        typename = x['type']
+        gamelist = x['gamelist']
+        linedata.append(deviceid)
+        linedata.append(packagename)
+        linedata.append(typename)
+        for key , value in gamelist.items():
+            line2 = linedata[:]
+            line2.append(str(key))
+            line2.append(str(value))
+            result.append(line2)
+        webgl_host_data['test_webgl_host'] = result
+    return webgl_host_data
 
 
+webgl_host_data = get_data()
