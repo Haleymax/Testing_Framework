ad41a82fe82ae3bbdb09cafe41d83525cb8073e7
hongwei.huang <hongwei.huang@unity.cn>
2024-10-10 15:28:02 +0800

    add feishu api

---
+++ core/feishu.py
@@ -0,0 +1,64 @@
+import time
+
+import requests
+
+from core.get_data import read_config
+from core.logger import logger
+
+
+class Feishu_request:
+    def __init__(self, spreadsheetToken, sheetId, access_token):
+        self.url = f'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{spreadsheetToken}/values/{sheetId}!{self.get_data_range()}'
+        logger.info(self.url)
+        self.headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+
+    def get_data_range(self):
+        start_column = read_config.get_start_column()
+        total_column = int(read_config.get_total_column())
+        start_code = ord(start_column)
+        end_code = start_code + total_column
+        end_column = chr(end_code)
+        range = str(start_column)+str(read_config.get_start_row())+":"+str(end_column)+str(read_config.get_total_row())
+        return range
+
+    def get_data_form_feishu(self):
+        logger.info("start request data form feishu api")
+        data = {}
+        retries = 4
+        delay = 2
+        for attempt in range(0, retries):
+            response = requests.get(self.url, headers=self.headers)
+            if response.status_code == 200:
+                data = response.json()
+            else:
+                logger.warning(f"attempt {attempt} request failed")
+                if attempt < retries - 1:
+                    time.sleep(delay)
+        return data
+
+
+
+    def create_insert_data(self):
+        request_data = self.get_data_form_feishu()
+        game_data = request_data['data']['valueRange']['values'][1:]
+        games = []
+        for data in game_data:
+            game = []
+            if data[0] is None or data[3] is None or data[4][0]['type'] != 'url':
+                continue
+            game.append(data[3])
+            game.append(data[4][0]['text'])
+            game.append(data[0])
+            games.append(game)
+        return games
+
+
+data_of_feishu = Feishu_request(read_config.get_spreadsheetToken(),read_config.get_sheetId(),read_config.get_access_token())
+
+data = data_of_feishu.create_insert_data()
+
+for result in data:
+    print(result)
